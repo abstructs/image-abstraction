741048b8c1d0badaa2827a9a8c0a9a8541bbd470
Andrew Wilson <andrewclarke540@gmail.com>
2016-04-14 18:38:27 +0000

    Add history feature and configure for c9

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
 var Search = require('bing.search');
-search = new Search('0HjNYFqn1WNVZznhnniSm7s2spsrztlhVgLCu8eSBpA');
+var search = new Search('0HjNYFqn1WNVZznhnniSm7s2spsrztlhVgLCu8eSBpA');
+
+var searchHistory = [
+  {"term":"lolcats","when":"2016-04-14T18:34:04.045Z"},
+  {"term":"coolio","when":"2016-04-14T18:34:09.707Z"}
+];
 
 app.get('/api/imagesearch/:query', function(req, res){
+  searchHistory.push({ "term": req.params.query, "when": new Date() });
   search.images(req.params.query,
     {top: 10,
      skip: req.query.offset},
     function(err, results) {
+      if (err) throw err;
       var newObj = results.map(function(result){
         return {
           "url": result.url,
@@ -16,9 +23,21 @@ app.get('/api/imagesearch/:query', function(req, res){
           "context": result.sourceUrl
         };
       });
-      res.end(JSON.stringify(newObj) + ' Offset: ' + req.query.offset);
+      res.end(JSON.stringify(newObj));
     }
   );
 });
 
-app.listen(3000, function(){ console.log('Server is running on port 3000...')});
+app.get('/', function(req, res){
+  searchHistory.map(function(history){
+    return {
+      "term": history.term,
+      "when": history.when
+    };
+  });
+  res.write('example usage: \n\nhttps://image-abstraction-polygoning.c9users.io/api/imagesearch/coolio?offset=3\n\n');
+  res.write('When you make a search your history will be added here: \n\n');
+  res.end(JSON.stringify(searchHistory));
+});
+
+app.listen(process.env.PORT || 8080, function(){ console.log('Server is running...')});
